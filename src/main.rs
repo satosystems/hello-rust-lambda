d240c0453137bb1cd4bc7216b7f7359c0e8d7620
Satoshi Ogata <satosystems@gmail.com>
2021-04-18 02:52:46 +0900

    Implement a lambda using lambda_runtime 0.2.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use serde::{Deserialize, Serialize};
+use std::error::Error as StdError;
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct CustomEvent {
+    family_name: String,
+    given_name: String,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct CustomOutput {
+    message: String,
+}
+
+fn main() -> Result<(), Box<dyn StdError>> {
+    lambda!(handler);
+    Ok(())
+}
+
+fn handler(event: CustomEvent, _context: Context) -> Result<CustomOutput, HandlerError> {
+    Ok(CustomOutput {
+        message: format!("Helli, {} {}!", event.given_name, event.family_name),
+    })
 }
